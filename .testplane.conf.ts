453c5cb5471b9df728cc80f80ad4f21ee2349a53
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-07-07 00:30:49 +0300

    make working testplane

--- .testplane.conf.ts
+++ .testplane.conf.ts
@@ -11,6 +11,18 @@ module.exports = {
             desiredCapabilities: {
                 browserName: "chrome",
             },
+            screenshotDelay: 200,
+        },
+        mobileChrome: {
+            automationProtocol: "devtools",
+            desiredCapabilities: {
+                browserName: "chrome",
+            },
+            windowSize: {
+                width: 400,
+                height: 700
+            },
+            screenshotDelay: 200,
         },
     },
 
