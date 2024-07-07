b0584df51fd8fd8e2d16cf0b8c9b7bda63b1844c
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-07-07 09:37:15 +0300

    one more test

--- test/testplane/catalog.testplane.ts
+++ test/testplane/catalog.testplane.ts
@@ -31,4 +31,25 @@ describe("Каталог", () => {
       expect(link).not.toBe(undefined);
     })
   });
+
+  it('На странице с подробной информацией отображаются: название товара, его описание, цена, цвет, материал и кнопка "добавить в корзину"', async ({browser}) => {
+    // подготовка
+    await browser.url("http://localhost:3000/hw/store/catalog/0");
+    const name = await browser.$(".ProductDetails-Name");
+    const desc = await browser.$(".ProductDetails-Description");
+    const price = await browser.$(".ProductDetails-Description");
+    const color = await browser.$(".ProductDetails-Color");
+    const material = await browser.$(".ProductDetails-Material");
+    const button = await browser.$(".ProductDetails-AddToCart");
+
+    // действие
+
+    // проверка
+    expect(name).not.toBe(undefined);
+    expect(desc).not.toBe(undefined);
+    expect(price).not.toBe(undefined);
+    expect(color).not.toBe(undefined);
+    expect(material).not.toBe(undefined);
+    expect(button).not.toBe(undefined);
+  });
 });
